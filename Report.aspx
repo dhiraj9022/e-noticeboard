7aedea0ba8b6a2acca6c67d0ac84fb9cf8bcaba5
dhiraj1999 <55016700+dhiraj1999@users.noreply.github.com>
2020-05-01 15:46:59 +0530

    Add files via upload

---
+++ Report.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Report.aspx.cs" Inherits="Report" %>
+
+<%@ Register assembly="CrystalDecisions.Web, Version=13.0.4000.0, Culture=neutral, PublicKeyToken=692fbea5521e1304" namespace="CrystalDecisions.Web" tagprefix="CR" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container-fluid">
+
+            <CR:CrystalReportViewer ID="CrystalReportViewer1" runat="server" AutoDataBind="true" EnableDatabaseLogonPrompt="False" ToolPanelView="None" />
+            <br />
+            <div  style="border-radius: 10px;text-align:center;padding-right:250PX;">
+                <asp:Button ID="btnback" runat="server" Text="Back" BackColor="#F76A8C" OnClick="btnback_Click" Height="30px" Width="80PX"/>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Report.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+
+public partial class Report : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        ReportDocument rdoc = new ReportDocument();
+        rdoc.Load(Server.MapPath("~/CrystalReport.rpt"));
+        DataSetNotice dsn = this.GetData("Select class,year,subject,date,message from Message");
+        rdoc.SetDataSource(dsn);
+        CrystalReportViewer1.ReportSource = rdoc;
+        CrystalReportViewer1.RefreshReport();
+
+    }
+
+    private DataSetNotice GetData(string query)
+    {
+        string cs = ConfigurationManager.ConnectionStrings["connectionstring"].ConnectionString;
+        SqlConnection con = new SqlConnection(cs);
+        con.Open();
+        SqlCommand cmd = new SqlCommand(query);
+        SqlDataAdapter da = new SqlDataAdapter(query,con);
+        DataSetNotice dsn = new DataSetNotice();
+        da.Fill(dsn,"Message");
+        return dsn;
+       
+    }
+    protected void btnback_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("http://localhost:49681/E-Notice Board1/AdminPage.aspx");
+    }
+}
